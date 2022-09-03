a07f398fc8b8ed1883628d9a5a1539d811b3f504
graz2003 <70001741+graz2003@users.noreply.github.com>
2022-09-03 13:08:27 -0400

    Add files via upload

--- sustainability.js
+++ sustainability.js
@@ -4,28 +4,31 @@ const correctMessage = "You are correct!";
 const incorrectMessage = "You are incorrect!";
 const displayScore = document.getElementById("scoreNum");
 const displaynewItem = document.getElementById("newItem");
-const displayResults = document.querySelector(".results");
-const displayExplain = document.getElementById("resultsExplain");
+const displayResults = document.querySelector(".resultsMessage");
+const displayExplain = document.querySelector(".resultsExplain");
 const choiceLandfill = document.getElementById("landfill");
 const choiceRecycling = document.getElementById("recycling");
 const choiceCompost = document.getElementById("compost");
 
 //Create list with all the images, the correct response and the reason they should be place in a specific bin 
-const itemsList = [["Banana.png", "compost", "this is because..."], ["Banana.png", "compost", "this is because..."]]
+const itemsList = [["Banana.png", "compost", "This is because bananas are a type of food waste. They are biodegradable and should go into the compost bin!"], ["PlasticBag.jpg", "landfill", "This is because plastic bags are not recyclable or compostable!"], ["TeaBag.jpg", "compost", "This is because tea bags are a type of organics!"], ["AluminiumCan.jpg", "recycling", "This is because aluminium cans can be recycled into other goods!"]];
 let index = 0;
-
-main();
 
 //Function that gives random number
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-//Function that chooses a random number from the itemList and shows it 
+//Function that chooses a random number from the itemList and shows it, does not exit the while loop unless the new index is different than the previous index 
 function newImage() {
-    index = randomInteger(0, itemsList.length - 1);
-    displaynewItem.innerHTML = itemsList[index][0];
-    
+    let temp = 0;
+    temp = index;
+
+    while (temp == index) {
+        index = randomInteger(0, itemsList.length - 1);
+    }
+
+    displaynewItem.src = itemsList[index][0];
 }
 
 //function that checks if the user input is the same as the right answer recorded in itemsList
@@ -37,32 +40,57 @@ function checkCorrect(input){
     }
 }
 
+
+main();
+
 function main() {
     newImage();
 
     //Event listeners for user response
     choiceLandfill.addEventListener('click', function() {
         if (checkCorrect("landfill") == true) {
-            score++; //increase score
+            score=score+10; //increase score
             displayScore.innerHTML = score; //display score
 
             displayResults.innerHTML = correctMessage; //display message
             displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
         } else {
             displayResults.innerHTML = incorrectMessage; //display message
             displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
         }
     })
 
     choiceRecycling.addEventListener('click', function(){
-        console.log("recycling");
+        if (checkCorrect("recycling") == true) {
+            score=score+10; //increase score
+            displayScore.innerHTML = score; //display score
+
+            displayResults.innerHTML = correctMessage; //display message
+            displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
+        } else {
+            displayResults.innerHTML = incorrectMessage; //display message
+            displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
+        }
     })
 
     choiceCompost.addEventListener('click', function(){
-        console.log("compost");
+        if (checkCorrect("compost") == true) {
+            score=score+10; //increase score
+            displayScore.innerHTML = score; //display score
+
+            displayResults.innerHTML = correctMessage; //display message
+            displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
+        } else {
+            displayResults.innerHTML = incorrectMessage; //display message
+            displayExplain.innerHTML = itemsList[index][2]; //display explaination
+            newImage();
+        }
     })
-
-
 
 }
 
